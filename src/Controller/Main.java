71177944605586e83e2f4ee94bac0abe1b5cc692
TranTuan0212 <tuantpde170492@fpt.edu.vn>
2023-11-04 09:50:20 +0700

    first commit

---
+++ src/Controller/Main.java
@@ -0,0 +1,13 @@
+package Controller;
+
+
+
+import Controller.Display;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Display d = new Display();
+        d.run();
+    }
+}
